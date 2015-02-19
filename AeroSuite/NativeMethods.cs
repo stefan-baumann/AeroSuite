2ff764392b5ca60db2fe956311c0e89e5ee1d2bf
Stefan Baumann <stefanbaumannbinzen@hotmail.de>
2015-02-19 16:28:35 +0100

    Create NativeMethods

---
+++ AeroSuite/NativeMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AeroSuite
+{
+    /// <summary>
+    /// Provides some methods from the user32 and uxtheme libraries.
+    /// </summary>
+    internal static class NativeMethods
+    {
+        private const string user32 = "user32.dll";
+        private const string uxtheme = "uxtheme.dll";
+
+        [DllImport(user32)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport(uxtheme, CharSet = CharSet.Unicode)]
+        public extern static int SetWindowTheme(IntPtr hWnd, string pszSubAppName, string pszSubIdList);
+    }
+}
