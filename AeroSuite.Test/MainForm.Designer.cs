63bc5f5f7e7efe59c8320f5554303763199a03a4
Stefan Baumann <stefanbaumannbinzen@hotmail.de>
2015-02-19 17:09:43 +0100

    Add Test Project

---
+++ AeroSuite.Test/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AeroSuite.Test
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.ControlPropertyGrid = new System.Windows.Forms.PropertyGrid();
+            this.TypeComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.ControlPropertyGrid);
+            this.splitContainer1.Panel2.Controls.Add(this.TypeComboBox);
+            this.splitContainer1.Size = new System.Drawing.Size(851, 530);
+            this.splitContainer1.SplitterDistance = 425;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // ControlPropertyGrid
+            // 
+            this.ControlPropertyGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ControlPropertyGrid.Location = new System.Drawing.Point(0, 21);
+            this.ControlPropertyGrid.Name = "ControlPropertyGrid";
+            this.ControlPropertyGrid.Size = new System.Drawing.Size(421, 509);
+            this.ControlPropertyGrid.TabIndex = 2;
+            // 
+            // TypeComboBox
+            // 
+            this.TypeComboBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TypeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TypeComboBox.FormattingEnabled = true;
+            this.TypeComboBox.Location = new System.Drawing.Point(0, 0);
+            this.TypeComboBox.Name = "TypeComboBox";
+            this.TypeComboBox.Size = new System.Drawing.Size(421, 21);
+            this.TypeComboBox.TabIndex = 1;
+            this.TypeComboBox.SelectedIndexChanged += new System.EventHandler(this.TypeComboBox_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(851, 530);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.Name = "MainForm";
+            this.Text = "AeroSuite Control Tester";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PropertyGrid ControlPropertyGrid;
+        private System.Windows.Forms.ComboBox TypeComboBox;
+    }
+}
+
---
+++ AeroSuite.Test/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AeroSuite.Test
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            var library = Assembly.Load("AeroSuite");
+            var exportedTypes = library.GetExportedTypes();
+            var controls = exportedTypes.Where(t => typeof(Control).IsAssignableFrom(t) && !typeof(Form).IsAssignableFrom(t));
+            this.TypeComboBox.DataSource = controls.ToList();
+            this.TypeComboBox.DisplayMember = "Name";
+            this.TypeComboBox.SelectedIndex = 0;
+        }
+
+        private Control currentControl;
+        private void TypeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.currentControl != null) this.currentControl.Dispose();
+            var type = (Type)this.TypeComboBox.SelectedItem;
+            this.splitContainer1.Panel1.Controls.Add(this.currentControl = (Control)Activator.CreateInstance(type));
+
+            this.currentControl.Text = type.Name;
+            this.currentControl.Location = new Point((this.splitContainer1.Panel1.Width - this.currentControl.Width) / 2, (this.splitContainer1.Panel1.Height - this.currentControl.Height) / 2);
+            this.currentControl.SizeChanged += (s, ea) => this.currentControl.Location = new Point((this.splitContainer1.Panel1.Width - this.currentControl.Width) / 2, (this.splitContainer1.Panel1.Height - this.currentControl.Height) / 2);
+
+            if (typeof(ITestControl).IsAssignableFrom(type))
+            {
+                (this.currentControl as ITestControl).PrepareForTests();
+            }
+
+
+            this.ControlPropertyGrid.SelectedObject = this.currentControl;
+        }
+    }
+}
