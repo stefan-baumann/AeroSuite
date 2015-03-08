d883f6ec3ce1e66d9a43342787c7d4541eb56ac3
Stefan Baumann <stefanbaumannbinzen@hotmail.de>
2015-03-08 12:29:34 +0100

    Remove not needed imports
    Removed "using System.Threading.Tasks" to make Travis work (builds
    didn't seem to be possible becasue System.Threading.Tasks was not found)

--- AeroSuite/Controls/AeroListView.cs
+++ AeroSuite/Controls/AeroListView.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AeroSuite.Controls
--- AeroSuite/PlatformHelper.cs
+++ AeroSuite/PlatformHelper.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms.VisualStyles;
 using Microsoft.Win32;
 
