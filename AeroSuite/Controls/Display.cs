144603d8d82b5d59970622a09bd501a1c1871bc4
Stefan Baumann <stefan-baumann@outlook.com>
2015-05-26 12:04:54 +0200

    Add View/Display-system
    Can be used similarly to a TabControl

---
+++ AeroSuite/Controls/Display.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AeroSuite.Controls
+{
+    /// <summary>
+    /// A host control for a <see cref="ViewBase"/>
+    /// </summary>
+    /// <remarks>
+    /// The perfect alternative to a <see cref="HeaderlessTabControl"/>: It works on every imaginable platform without any bugs but also unfolds the possibility to create advanced single-window applications with a lot of OOP-compliance as you can easily create a class per View.
+    /// </remarks>
+    [DesignerCategory("Code")]
+    [DisplayName("Display")]
+    [Description("A host control for Views.")]
+    [ToolboxItem(true)]
+    [ToolboxBitmap(typeof(Panel))]
+    public class Display
+        : ContainerControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Display"/> class.
+        /// </summary>
+        public Display()
+        {
+            this.Dock = DockStyle.Fill;
+            this.AutoScroll = true;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.Forms.Control.Paint" /> event.
+        /// </summary>
+        /// <param name="e">A <see cref="T:System.Windows.Forms.PaintEventArgs" /> that contains the event data.</param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (this.DesignMode)
+            {
+                e.Graphics.DrawString(this.Name + "\n Specify a View in the View property to display it.", this.Font, SystemBrushes.ControlDarkDark, new Rectangle(0, 0, this.Width, this.Height), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center, Trimming = StringTrimming.EllipsisCharacter });
+            }
+
+            base.OnPaint(e);
+        }
+
+        /// <summary>
+        /// Occurs when the displayed View changed.
+        /// </summary>
+        public event EventHandler ViewChanged;
+
+        private ViewBase view;
+        /// <summary>
+        /// Gets or sets the view displayed.
+        /// </summary>
+        /// <value>
+        /// The view.
+        /// </value>
+        public virtual ViewBase View
+        {
+            get
+            {
+                return this.view;
+            }
+            set
+            {
+                if (value == this.view)
+                {
+                    return;
+                }
+
+                if (this.view != null)
+                {
+                    this.view.ClearDisplayInternal();
+                }
+
+                this.SetViewInternal(value, true);
+            }
+        }
+
+        /// <summary>
+        /// Clears the view internally.
+        /// </summary>
+        internal virtual void ClearViewInternal()
+        {
+            if (this.view != null && this.Controls.Contains(this.view))
+            {
+                this.Controls.Remove(this.view);
+            }
+
+            this.view = null;
+        }
+
+        /// <summary>
+        /// Sets the view internally.
+        /// </summary>
+        /// <param name="view">The new view for this display.</param>
+        /// <param name="notifyView">if set to <c>true</c> the view will be notified so that it can adapt to the change.</param>
+        internal virtual void SetViewInternal(ViewBase view, bool notifyView)
+        {
+            this.view = view;
+
+            if (this.view != null)
+            {
+                if (notifyView)
+                {
+                    this.view.SetDisplayInternal(this, false);
+
+                    if (this.ViewChanged != null)
+                    {
+                        this.ViewChanged(this, EventArgs.Empty);
+                    }
+                }
+
+                this.Controls.Add(this.view);
+            }
+        }
+    }
+}
+
---
+++ AeroSuite/Controls/ViewBase.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AeroSuite.Controls
+{
+    /// <summary>
+    /// <para>A base class for creating Views that can be displayed in a <see cref="Display"/>.</para>
+    /// <para>There are three ways for creating Views:</para>
+    /// <para>1.: Create a file of type "Inherited User Control" (you can do it by pressind Crtl+Shift+A in Visual Studio and then selecting "Windows Forms > Inherited User Control"; then select <see cref="VeiwBase"/> as the class to inherit from)</para>
+    /// <para>2.: Create a new form or UserControl and edit the code so that it inherits from <see cref="ViewBase"/>.</para>
+    /// <para>3.: Create a new class and make it inherit from <see cref="ViewBase"/>. However, Designer Code will be generated in the main code file.</para>
+    /// </summary>
+    /// <remarks>
+    /// Combined with a <see cref="Display"/>, this is the perfect alternative to the <see cref="HeaderlessTabControl"/>.
+    /// </remarks>
+    [DesignerCategory("Code")]
+    [DisplayName("Shield Button")]
+    [Description("A normal button with a Shield.")]
+    [ToolboxItem(true)]
+    [ToolboxBitmap(typeof(TabPage))]
+    public class ViewBase
+        : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewBase"/> class.
+        /// </summary>
+        public ViewBase()
+        {
+            this.BackColor = SystemColors.Window;
+            this.Font = SystemFonts.MessageBoxFont;
+            this.Size = new Size(750, 500);
+
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        private Display display;
+        /// <summary>
+        /// Gets or sets the display.
+        /// </summary>
+        /// <value>
+        /// The display.
+        /// </value>
+        public virtual Display Display
+        {
+            get
+            {
+                return this.display;
+            }
+            set
+            {
+                if (value == this.display)
+                {
+                    return;
+                }
+
+                if (this.UsedInMultiViewDisplay)
+                {
+                    this.display = value;
+                }
+                else
+                {
+                    if (this.display != null)
+                    {
+                        this.display.ClearViewInternal();
+                    }
+                    
+                    this.SetDisplayInternal(value, true);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this view is used in an MultiViewDisplay.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if used in an MultiViewDisplay; otherwise, <c>false</c>.
+        /// </value>
+        internal virtual bool UsedInMultiViewDisplay { get; set; }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.Forms.ContainerControl.OnParentChanged" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> that contains the event data.</param>
+        /// <exception cref="System.InvalidOperationException">A View cannot be put in anything other than a Display.</exception>
+        protected override void OnParentChanged(EventArgs e)
+        {
+            if (!this.DesignMode && !(this.Parent is Display))
+            {
+                throw new InvalidOperationException("A View cannot be put in anything other than a Display.");
+            }
+            MessageBox.Show(this.Parent.ToString());
+
+            base.OnParentChanged(e);
+        }
+
+        /// <summary>
+        /// Clears the display internal.
+        /// </summary>
+        internal virtual void ClearDisplayInternal()
+        {
+            this.display = null;
+        }
+
+        /// <summary>
+        /// Sets the display internally.
+        /// </summary>
+        /// <param name="display">The new display this view will be displayed in.</param>
+        /// <param name="notifyDisplay">if set to <c>true</c> the display will be notified so that it can adapt to the change.</param>
+        internal virtual void SetDisplayInternal(Display display, bool notifyDisplay)
+        {
+            this.display = display;
+
+            if (this.display != null)
+            {
+                if (notifyDisplay)
+                {
+                    this.display.SetViewInternal(this, false);
+                }
+            }
+        }
+    }
+}
+
